a01fb951717d3f95e057bcaf5f36f0d5e8d34f29
Satrio Bagus <satrio.baguswbw@gmail.com>
2026-05-16 02:55:59 +0700

    add: home stat card

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { Database } from '@lucide/vue';
+import { RouterLink } from 'vue-router';
+</script>
 <template>
-  <h1>Home Page</h1>
+  <div class="flex flex-wrap">
+    <div class="w-1/4 p-3">
+      <RouterLink to="/template" class="w-full bg-base-200 p-5 flex items-center shadow-lg">
+        <div><Database :size="84" /></div>
+        <div class="pl-5 font-semibold text-xl">Total Data: 62</div>
+      </RouterLink>
+    </div>
+  </div>
 </template>
